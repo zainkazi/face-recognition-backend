f2c7142cc74820ca125c32edf291ba47724374a5
Zain <zainkazi27@gmail.com>
2022-08-24 22:13:18 +0500

    adding files to repo

---
+++ server.js
@@ -0,0 +1,115 @@
+const express = require("express");
+const cors = require("cors");
+const bcrypt = require("bcrypt");
+const app = express();
+const knex = require("knex")({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    port: 5432,
+    user: "postgres",
+    password: "1234",
+    database: "facerecognition",
+  },
+});
+
+app.use(express.json());
+app.use(cors());
+
+const saltRounds = 10;
+
+app.get("/", (req, res) => {
+  res.json(database.users);
+});
+
+app.post("/signin", (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json("Incorrect from data");
+  }
+  knex
+    .select("email", "hash")
+    .from("login")
+    .where("email", "=", email)
+    .then((data) => {
+      const isValid = bcrypt.compareSync(password, data[0].hash);
+      console.log(isValid);
+      console.log(data[0].hash);
+      console.log(password);
+      if (isValid) {
+        return knex
+          .select("*")
+          .from("users")
+          .where("email", "=", email)
+          .then((user) => {
+            res.json(user[0]);
+          })
+          .catch((err) => res.status(400).json("unable to get user"));
+      } else {
+        res.status(400).json("wrong credentials");
+      }
+    })
+    .catch((err) => res.status(400).json("wrong credentials"));
+});
+
+app.post("/register", (req, res) => {
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    return res.status(400).json("Incorrect from data");
+  }
+  const hash = bcrypt.hashSync(password, saltRounds);
+
+  knex
+    .transaction((trx) => {
+      trx
+        .insert({
+          hash: hash,
+          email: email,
+        })
+        .into("login")
+        .returning("email")
+        .then((loginEmail) => {
+          trx("users")
+            .returning("*")
+            .insert({
+              name: name,
+              email: loginEmail[0].email,
+              joined: new Date(),
+            })
+            .then((user) => res.json(user[0]));
+        })
+        .then(trx.commit)
+        .catch(trx.rollback);
+    })
+    .catch((err) => res.status(400).json("unable to register"));
+});
+
+app.get("/profile/:id", (req, res) => {
+  const { id } = req.params;
+  knex
+    .select("*")
+    .from("users")
+    .where({ id })
+    .then((user) => {
+      if (user.length) {
+        res.json(user[0]);
+      } else {
+        res.status(404).json("no user with such id");
+      }
+    })
+    .catch((err) => res.status(400).json("Error getting user"));
+});
+
+app.put("/image", (req, res) => {
+  const { id } = req.body;
+  knex("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then((entries) => res.json(entries[0].entries))
+    .catch((err) => res.status(400).json("error getting entries"));
+});
+
+app.listen(3001, () => {
+  console.log("Listening on port 3001...");
+});
